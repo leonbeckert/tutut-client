39a2687212dac14e903ce0d2511864a5fd05927b
Leon Beckert <leon.beckert@posteo.de>
2020-10-07 17:56:47 +0200

    Added Redux

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
 import themeFile from "./util/theme";
 import jwtDecode from "jwt-decode";
 
+// Redux
+import { Provider } from "react-redux";
+import store from "./redux/store";
 // Components
 import NavBar from "./components/Navbar";
 import AuthRoute from "./util/AuthRoute";
@@ -33,7 +36,7 @@ class App extends Component {
     render() {
         return (
             <MuiThemeProvider theme={theme}>
-                <div className="App">
+                <Provider store={store}>
                     <Router>
                         <NavBar />
                         <div className="container">
@@ -54,7 +57,7 @@ class App extends Component {
                             </Switch>
                         </div>
                     </Router>
-                </div>
+                </Provider>
             </MuiThemeProvider>
         );
     }
