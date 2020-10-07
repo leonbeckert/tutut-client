84cd5e107d3d0beecc95f08b11fb91086e60acf9
Leon Beckert <leon.beckert@posteo.de>
2020-10-07 11:13:38 +0200

    Added post card details

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,39 @@
 import React, { Component } from "react";
+import axios from "axios";
+import Grid from "@material-ui/core/Grid";
+
+import Tut from "../components/Tut";
 
 export class home extends Component {
+    state = {
+        tuts: null,
+    };
+    componentDidMount() {
+        axios
+            .get("/tuts")
+            .then((res) => {
+                console.log(res.data);
+                this.setState({
+                    tuts: res.data,
+                });
+            })
+            .catch((err) => console.error(err));
+    }
     render() {
+        let recentTutsMarkup = this.state.tuts ? (
+            this.state.tuts.map((tut) => <Tut tut={tut} />)
+        ) : (
+            <p>Loading...</p>
+        );
         return (
-            <div>
-                <h1>Home page</h1>
-            </div>
+            <Grid container spacing={16}>
+                <Grid item sm={8} xs={12}>
+                    {recentTutsMarkup}
+                </Grid>
+                <Grid item sm={4} xs={12}>
+                    <p>Profile...</p>
+                </Grid>
+            </Grid>
         );
     }
 }
