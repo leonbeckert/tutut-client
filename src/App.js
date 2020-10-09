065989bb07681bc48e5b00fed8783bdc3de52a09
Leon Beckert <leon.beckert@posteo.de>
2020-10-09 15:02:56 +0200

    Updated login and signup authentication

--- src/App.js
+++ src/App.js
@@ -3,32 +3,37 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import { ThemeProvider as MuiThemeProvider } from "@material-ui/core/styles";
 import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
-import themeFile from "./util/theme";
 import jwtDecode from "jwt-decode";
 
 // Redux
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import { SET_AUTHENTICATED } from "./redux/types";
+import { logoutUser, getUserData } from "./redux/actions/userActions";
+
 // Components
 import NavBar from "./components/Navbar";
 import AuthRoute from "./util/AuthRoute";
+import themeFile from "./util/theme";
 
 // Pages
 import home from "./pages/home";
 import login from "./pages/login";
 import signup from "./pages/signup";
+import axios from "axios";
 
 const theme = createMuiTheme(themeFile);
 
-let authenticated;
 const token = localStorage.FBIdToken;
 if (token) {
     const decodedToken = jwtDecode(token);
     if (decodedToken.exp * 1000 < Date.now()) {
-        authenticated = false;
+        store.dispatch(logoutUser);
         window.location.href = "/login";
     } else {
-        authenticated = true;
+        store.dispatch({ type: SET_AUTHENTICATED });
+        axios.defaults.headers.common["Authorization"] = token;
+        store.dispatch(getUserData());
     }
 }
 
@@ -46,13 +51,11 @@ class App extends Component {
                                     exact
                                     path="/login"
                                     component={login}
-                                    authenticated={authenticated}
                                 />
                                 <AuthRoute
                                     exact
                                     path="/signup"
                                     component={signup}
-                                    authenticated={authenticated}
                                 />
                             </Switch>
                         </div>
