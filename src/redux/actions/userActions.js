c114f533ce556577de2c7e164261a094fe5905bb
Leon Beckert <leon.beckert@posteo.de>
2020-10-09 17:26:15 +0200

    Image upload works

--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -63,6 +63,16 @@ export const getUserData = () => (dispatch) => {
         .catch((err) => console.error(err));
 };
 
+export const uploadImage = (formData) => (dispatch) => {
+    dispatch({ type: LOADING_USER });
+    axios
+        .post("/user/image", formData)
+        .then((res) => {
+            dispatch(getUserData());
+        })
+        .catch((err) => console.log(err));
+};
+
 export const setAuthorizationHeader = (token) => {
     const FBIdToken = `Bearer ${token}`;
     localStorage.setItem("FBIdToken", FBIdToken);
