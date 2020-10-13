6f79659e27fc99ee224fea5a343ba58cbbfc9d45
Leon Beckert <leon.beckert@posteo.de>
2020-10-13 19:20:06 +0200

    Deployed to Firebase

--- src/App.js
+++ src/App.js
@@ -26,6 +26,9 @@ import axios from "axios";
 
 const theme = createMuiTheme(themeFile);
 
+axios.defaults.baseURL =
+    "https://europe-west3-tutut-2e188.cloudfunctions.net/api";
+
 const token = localStorage.FBIdToken;
 if (token) {
     const decodedToken = jwtDecode(token);
